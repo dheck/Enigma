dheck/Enigma

commit a44b59a6b9b6850a6e5b4b96386a17ba4503c422

Trunk 1.1: internal reengineering - abolish get_kind(), is_kind() with their mixture of class, variation and     model name results - simulate old API GetKind in api1init by addition of a GetClass function - do no longer store kind as an object attribute - resolve kind always via object validator - prefer usage of getClass where possible - introduce getModelName - currently used model of an object     - support all static floors in all variations and most other objects     - used by st_chameleon     - used by default init_model()     - objects with own init_model do not yet support this method - all floors test level: add st_chameleon vs. floor test Note: - this patch affects most objects in essential parts - please report any   unexpected behaviour changes
git-svn-id: svn://svn.berlios.de/enigma-game/trunk@1793 02f23ccc-97f9-0310-b3a5-cbfa8986ee20

data/api1init.lua
@@ -701,8 +701,12 @@ function enigma.MakeObject(name)
 end
 
 function enigma.GetKind(obj)
-    local _newname = enigma._GetKind(obj)
+    local _newname = enigma.GetClass(obj)
     local _oldname = RenamingObjectsNew2Old[_newname]
+    
+    if _oldname == nil then
+        _oldname = RenamingObjectsNew2Old[enigma._GetKind(obj)]
+    end
 
     if _newname == "st_key" then
         local code = enigma._GetAttrib(obj, "code")

src/player.cc
@@ -261,7 +261,7 @@ bool player::WieldedItemIs (Actor *a, const string &kind)
 {
     if (Inventory *inv = GetInventory(a))
         if (Item *it = inv->get_item(0))
-            return it->is_kind(kind);
+            return it->isKind(kind);
     return false;
 }
 
@@ -581,23 +581,19 @@ void player::PickupItem (Actor *a, GridPos p)
     }
 }
 
-void player::PickupStoneAsItem (Actor *a, enigma::GridPos p) 
-{
-    if (Inventory *inv = MayPickup(a, GetField(p)->item, true)) {
-        if (Stone *stone = GetStone(p))  {
-            string kind = stone->get_kind();
-            if (kind[0] == 's') 
-                kind[0] = 'i';
-
-            if (Item *item = MakeItem(kind.c_str())) {
-                inv->add_item(item);
-                stone->transferIdentity(item);
-                KillStone(p);
-                player::RedrawInventory(inv);
-                sound::EmitSoundEvent ("pickup", p.center());
-            }
+bool player::PickupAsItem(Actor *a, GridObject *obj, std::string kind) {
+    if (Item *item = MakeItem(kind.c_str())) {
+        if (Inventory *inv = MayPickup(a, item, true)) {
+            inv->add_item(item);
+            obj->transferIdentity(item);
+            player::RedrawInventory(inv);
+            sound::EmitSoundEvent("pickup", a->get_pos());
+            return true;
+        } else {
+            DisposeObject(item);
         }
     }
+    return false;
 }
 
 void player::ActivateFirstItem() 

commit 1fe4626c2fcb425bf101fa4aea1d093548e7cd1c

Trunk 1.1: internal reengineering - add missing simple item object.xml declarations that caused problems with     r1793 - reengineer actor essential attribute according refman,     - keep old API compatible - fix ConserveLevel=false, that caused 2 extralifes on first death
git-svn-id: svn://svn.berlios.de/enigma-game/trunk@1796 02f23ccc-97f9-0310-b3a5-cbfa8986ee20

data/api1init.lua
@@ -1021,6 +1021,13 @@ function enigma.SetAttrib(obj, key, val)
      if key == "player" then
          _key = "owner"
      end
+     if key == "essential" then
+        if val == -1 then
+            _val = 1
+        elseif val == 1 then
+            _val = 2
+        end
+     end
      enigma._SetAttrib(obj, _key, _val)
 end
 
@@ -1157,6 +1164,13 @@ function enigma.GetAttrib(obj, key)
      end
      if key == "gohome" then
          if val == false then val = 0 else val = 1 end
+     end
+     if key == "essential" then
+        if val == 1 then
+            val = -1
+        elseif val == 2 then
+            val = 1
+        end
      end
      return val
 end

data/schemas/objects.xml
@@ -853,11 +853,6 @@
     </object>
     <object name="it_brake"/>
     <object name="it_brush"/>
-    <object name="it_cherry"/>
-    <object name="it_coin">
-      <attr name="coin_value" default="3"/>
-      <attr name="state" rw="r"/>
-    </object>
     <object name="it_burnable">
       <attr name="state" max="5" rw="r"/>
       <msg name="extinguish"/>
@@ -882,6 +877,12 @@
     </object>
     <object name="it_burnable_fireproof">
       <attr name="state" value="5"/>
+    </object>
+    <object name="it_cherry"/>
+    <object name="it_coffee"/>
+    <object name="it_coin">
+      <attr name="coin_value" default="3"/>
+      <attr name="state" rw="r"/>
     </object>
     <object name="it_coin_s">
       <attr name="state" value="0"/>
@@ -922,6 +923,7 @@
     <object name="it_document">
       <attr name="text"/>
     </object>
+    <object name="it_debris"/>
     <object name="it_drop"/>
     <object name="it_extinguisher">
       <attr name="state" max="2"/>
@@ -963,7 +965,6 @@
     </object>
     <object name="it_extralife"/>
     <object name="it_extralife_new" init="true"/>
-    <object name="it_floppy"/>
     <object name="it_flag">
       <attr name="color"/>
     </object>
@@ -973,6 +974,7 @@
     <object name="it_flag_white">
       <attr name="color" value="1"/>
     </object>
+    <object name="it_floppy"/>
     <object name="it_glasses">
       <attr name="state" max="31"/>
     </object>
@@ -985,6 +987,7 @@
       <attr name="code"/>
     </object>
     <object name="it_landmine"/>
+    <object name="it_magicwand"/>
     <object name="it_magnet">
       <attr name="range" default="nil"/>
       <attr name="strength" default="nil"/>
@@ -1077,6 +1080,7 @@
     <object name="it_puller_n">
       <attr name="orientation" value="3"/>
     </object>
+    <object name="it_ring"/>
     <object name="it_rubberband">
       <attr name="anchor2"/>
       <attr name="strength" default="10"/>
@@ -1123,6 +1127,7 @@
     <object name="it_shogun_l">
       <attr name="flavor" value="l"/>
     </object>
+    <object name="it_spoon"/>
     <object name="it_spring">
     </object>
     <object name="it_spring_keep">
@@ -1135,6 +1140,7 @@
     <object name="it_squashed">
       <msg name="_brush"/>
     </object>
+    <object name="it_squashed"/>
     <object name="it_strip">
       <attr name="connections"/>
       <attr name="friction"/>
@@ -1240,6 +1246,7 @@
     <object name="it_wrench"/>
     <object name="ot" abstract="true">
     </object>
+    <object name="it_yinyang"/>
     <object name="ot_counter">
       <msg name="signal"/>
     </object>

src/items/SimpleItems.hh
@@ -172,14 +172,14 @@ namespace enigma {
     DEF_ITEM(Floppy, "it_floppy", it_floppy);
 
     /**
-     * MagicWand
-     */
-    DEF_ITEM(MagicWand, "it_magicwand", it_magicwand);
-
-    /**
      * Key
      */
     DEF_ITEM(Key, "it_key", it_key);
+
+    /**
+     * MagicWand
+     */
+    DEF_ITEM(MagicWand, "it_magicwand", it_magicwand);
 
     /**
      * Pencil

src/player.cc
@@ -431,7 +431,7 @@ void player::CheckDeadActors() {
                 essId = a->get_traits().name;
             int essential = a->getAttr("essential");
             // count number of necessary actors per kind
-            if (essential == 1)
+            if (essential == 2)
                 --essMap[essId];
 
             if (!a->is_dead() ||
@@ -441,12 +441,12 @@ void player::CheckDeadActors() {
                     has_living_actor = true;
                 }
                 // count number of alive actors per kind
-                if (essential >= 0)
+                if (essential == 0 || essential == 2)
                     ++essMap[essId];
             }
             else {
                 // player is dead and could not resurrect
-                if (essential == -1) {
+                if (essential == 1) {
                     // actor is personnally essential but dead
                     new_game = true;
                 }

src/server.cc
@@ -156,8 +156,8 @@ void load_level(lev::Proxy *levelProxy, bool isRestart)
             sound::StartLevelMusic(false);
             
         server::LoadedProxy = levelProxy;
-        server::PrepareLevel();
         IsLevelRestart = isRestart;
+        server::PrepareLevel();
 
         // clear inventory before level load and give us 2 extralives
         player::NewGame();